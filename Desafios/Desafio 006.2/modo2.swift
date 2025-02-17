97ad9f1d0b5a6bcc417c229aaea6930aba4e890e
Emanoel Borges <rodbemanoelstudies@gmail.com>
2025-02-17 09:50:22 -0300

    Create modo2.swift
    Modo 2.

---
+++ Desafios/Desafio 006.2/modo2.swift
@@ -0,0 +1,18 @@
+//
+//  modo2.swift
+//  Desafio06Ex2
+//
+//  Created by Turma01-10 on 11/02/25.
+//
+
+import SwiftUI
+
+struct modo2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    modo2()
+}

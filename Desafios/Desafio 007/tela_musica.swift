26b81992b28640953d95a7e1cc3c9ada97c34187
Emanoel Borges <rodbemanoelstudies@gmail.com>
2025-02-17 09:55:27 -0300

    Create tela_musica.swift
    Tela da música.

---
+++ Desafios/Desafio 007/tela_musica.swift
@@ -0,0 +1,21 @@
+//
+//  tela1.swift
+//  Desafio07
+//
+//  Created by Turma01-10 on 12/02/25.
+//
+
+import SwiftUI
+
+struct tela1: View {
+    
+    @State var recebe : Array = []
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tela1()
+}

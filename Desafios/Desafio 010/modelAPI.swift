887c495a1e8960646c6df472bafddca053a1b6fd
Emanoel Borges <rodbemanoelstudies@gmail.com>
2025-02-21 08:19:52 -0300

    Create modelAPI.swift
    API.

---
+++ Desafios/Desafio 010/modelAPI.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  Desafio10
+//
+//  Created by Turma01-10 on 03/02/25.
+//
+
+import Foundation
+
+struct Pessoa: Codable, Hashable {
+  //  let id : String
+    let nome :  String?
+    let sobrenome :  String?
+    let idade :  Int?
+}

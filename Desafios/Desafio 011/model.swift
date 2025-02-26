9b2a2967bc19629483ad18b3b6a2b6c82863fd04
Emanoel Borges <rodbemanoelstudies@gmail.com>
2025-02-26 11:31:52 -0300

    Create model.swift
    Api umidade.

---
+++ Desafios/Desafio 011/model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  Desafio11
+//
+//  Created by Turma01-10 on 24/02/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct Umidade: Decodable, Hashable {
+    let umidade: String?
+    let data: Int?
+}
